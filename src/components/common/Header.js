328fda3adf356af57f1478dc131b1ad683bd5164
Tali Avni <tali@aristo7.com>
2020-08-01 15:55:33 -0700

    fixed header

---
+++ src/components/common/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.css';
+
+export const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <h1>Music Search</h1>
+    </header>
+  );
+};
+
+export default Header;
