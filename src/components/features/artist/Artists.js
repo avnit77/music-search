13cf1ec771dfcc6def321b58854e1ed47ea5a221
Tali Avni <tali@aristo7.com>
2021-09-08 13:47:56 -0700

    remove artist component

--- src/components/features/artist/Artists.js
+++ src/components/features/artist/Artists.js
@@ -1,25 +1,21 @@
 import React from "react";
-import { Artist } from "./Artist";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import Results from '../../elem/Results'
 
 export const Artists = ({ artists }) => {
   const artistsElements = artists.map((artist) => {
     return (
       <li key={artist.id}>
         <Link to={`/artists/${artist.name}/${artist.id}`}>
-          <Artist name={artist.name} />
+          <h1>{artist.name}</h1>
         </Link>
       </li>
     );
   });
 
   return (
-    <div className="results">
-      <ul>
-        <h2>{artistsElements}</h2>
-      </ul>
-    </div>
+<Results items={artistsElements} />
   );
 };
 
